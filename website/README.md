aeb5006c9760ed17d03d801fce667ccf05a578f9
Ace Nassri <anassri@google.com>
2021-05-21 14:12:55 -0700

    feat(website): create hello-world Flask app (#15)
    * Create hello-world website
    
    * Address comments
    
    * Address (default config) lint errors
    
    * Use snake_case
    
    * flake8 -> black
    
    * Add app.run()

---
+++ website/README.md
@@ -0,0 +1,21 @@
+# Emblem - Website
+
+## Platform
+This website is written using the
+[Flask](https://flask.palletsprojects.com/en/2.0.x/) web framework for Python.
+
+## Setup
+To run this project, you'll need the following tools:
+
+* Python (3.8 or higher)
+* [pip](https://pypi.org/project/pip/)
+
+The following tools are optional, but recommended:
+
+* [pyenv](https://github.com/pyenv/pyenv)
+
+To install dependencies for the website, `cd` into the `website` directory and
+run `pip install -r requirements.txt`.
+
+## Running
+To run the website, use the `flask run` command.
---
+++ website/app.py
@@ -0,0 +1,47 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import datetime
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# TODO(anassri, engelke): use API call instead of this
+# (This is based on the API design doc for now.)
+
+SAMPLE_CAMPAIGNS = [
+    {
+        "name": "cash for camels",
+        "goal": 2500,
+        "managers": ["Chris the camel", "Carissa the camel"],
+        "updated": datetime.date(2021, 2, 1),
+    },
+    {
+        "name": "water for fish",
+        "goal": 2500,
+        "managers": [
+            "Fred the fish",
+        ],
+        "updated": datetime.date(2021, 5, 10),
+    },
+]
+
+
+@app.route("/")
+def webapp_list_campaigns():
+    return render_template("home.html", campaigns=SAMPLE_CAMPAIGNS)
+
+
+app.run()
