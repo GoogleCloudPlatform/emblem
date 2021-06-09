3ab7087ebfbd5333319da55236491060fc95dd70
Dina Graves Portman <dinagraves@google.com>
2021-06-09 16:10:02 -0700

    Nit fix.
    Co-authored-by: Ace Nassri <anassri@google.com>

--- website/app.py
+++ website/app.py
@@ -74,5 +74,5 @@ if __name__ == "__main__":
     PORT = int(os.getenv("PORT")) if os.getenv("PORT") else 8080
 
     # This is used when running locally. Gunicorn is used to run the
-    # application on Cloud Run. See entrypoint in Dockerfile.
+    # application on Cloud Run; see "entrypoint" parameter in the Dockerfile.
     app.run(host="127.0.0.1", port=PORT, debug=True)
