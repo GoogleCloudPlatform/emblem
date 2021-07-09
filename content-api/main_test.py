d5076ff66b7859657f7112d03e5abbcd9008b8b7
Charles Engelke <engelke@google.com>
2021-07-09 16:24:44 -0700

    Api cleanup (#92)
    * Markdown API docs
    
    * Refactored all resource methods to one package
    
    * Restored accidentally deleted resources/base
    
    * Partial separation of db access from methods
    
    * DB separated from method handlers
    
    * Added first test
    
    * More tests
    
    * API functionally complete per current spec
    
    * Reformatted by black
    
    * Delete ,
    
    Inadvertent rename, later restored
    
    * Address PR #92 comments
    
    * Used existing helper method more often
    
    * Respond to PR review

---
+++ content-api/main_test.py
@@ -0,0 +1,169 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import pytest
+
+import main
+from resources import methods
+
+
+kinds = [key for key in methods.resource_fields]
+
+
+@pytest.fixture
+def client():
+    main.app.testing = True
+    return main.app.test_client()
+
+
+# Can we list every kind of resource?
+def test_list(client):
+    for kind in kinds:
+        r = client.get("/{}".format(kind))
+        assert r.status_code == 200
+        assert r.headers.get("Content-Type") == "application/json"
+        payload = json.loads(r.data)
+        assert type(payload) == list
+
+
+# Create, fetch, modify, and delete resources
+def test_lifecycle(client):
+    for kind in kinds:
+        if kind == "donations":  # Special case for later
+            continue
+
+        # Create a resource. Note that only the name field is mandatory
+        representation = {"name": "test name"}
+        r = client.post("/{}".format(kind), json=representation)
+        assert r.status_code == 201
+        resource = r.get_json(r.data)
+        assert type(resource) == dict
+        assert resource["name"] == representation["name"]
+
+        # Check that the id is in the list response
+        etag, _ = r.get_etag()
+        id = resource["id"]
+        r = client.get("/{}".format(kind))
+        assert r.status_code == 200
+        payload = r.get_json(r.data)
+        found = False
+        for item in payload:
+            if item["id"] == id:
+                found = True
+                break
+        assert found
+
+        # Update only if same etag, given wrong etag
+        representation = {"name": "changed name"}
+        r = client.patch(
+            "/{}/{}".format(kind, id),
+            json=representation,
+            headers={"If-Match": "wrong data"},
+        )
+        assert r.status_code == 409
+
+        # Update only if same etag, given right etag
+        representation = {"name": "changed name"}
+        r = client.patch(
+            "/{}/{}".format(kind, id), json=representation, headers={"If-Match": etag}
+        )
+        assert r.status_code == 201
+        resource = r.get_json(r.data)
+        assert type(resource) == dict
+        assert resource["name"] == representation["name"]
+
+        # Fetch updated resource
+        r = client.get("/{}/{}".format(kind, id))
+        assert r.status_code == 200
+        resource = r.get_json(r.data)
+        assert type(resource) == dict
+        assert resource["name"] == representation["name"]
+        new_etag, _ = r.get_etag()
+
+        # Try to delete, given wrong etag
+        r = client.delete("/{}/{}".format(kind, id), headers={"If-Match": "wrong"})
+        assert r.status_code == 409
+
+        # Try to delete, given correct etag
+        r = client.delete("/{}/{}".format(kind, id), headers={"If-Match": new_etag})
+        assert r.status_code == 204
+
+        # Try to fetch deleted resource
+        r = client.get("/{}/{}".format(kind, id))
+        assert r.status_code == 404
+
+
+# Create a campaign and donor, and then a donation for them
+def test_donation(client):
+    # Create a campaign
+    campaign_representation = {"name": "test campaign"}
+    r = client.post("/{}".format("campaigns"), json=campaign_representation)
+    assert r.status_code == 201
+    campaign = r.get_json(r.data)
+    assert type(campaign) == dict
+    assert campaign["name"] == campaign_representation["name"]
+
+    # Create a donor
+    donor_representation = {"name": "test donor"}
+    r = client.post("/{}".format("donors"), json=donor_representation)
+    assert r.status_code == 201
+    donor = r.get_json(r.data)
+    assert type(donor) == dict
+    assert donor["name"] == donor_representation["name"]
+
+    # Create the only donation for that campaign or donor
+    donation_representation = {
+        "campaign": campaign["id"],
+        "donor": donor["id"],
+        "amount": 50,
+    }
+    r = client.post("/{}".format("donations"), json=donation_representation)
+    assert r.status_code == 201
+    donation = r.get_json(r.data)
+    assert type(donation) == dict
+    assert donation["campaign"] == donation_representation["campaign"]
+    assert donation["donor"] == donation_representation["donor"]
+
+    # List of donations to campaign should have exactly one element
+    r = client.get("/campaigns/{}/donations".format(campaign["id"]))
+    assert r.status_code == 200
+    assert r.headers.get("Content-Type") == "application/json"
+    payload = json.loads(r.data)
+    assert type(payload) == list
+    assert len(payload) == 1
+    resource = payload[0]
+    assert resource["amount"] == 50
+    assert resource["campaign"] == campaign["id"]
+    assert resource["donor"] == donor["id"]
+
+    # List of donations from donor should have exactly one element
+    r = client.get("/donors/{}/donations".format(donor["id"]))
+    assert r.status_code == 200
+    assert r.headers.get("Content-Type") == "application/json"
+    payload = json.loads(r.data)
+    assert type(payload) == list
+    assert len(payload) == 1
+    resource = payload[0]
+    assert resource["amount"] == 50
+    assert resource["campaign"] == campaign["id"]
+    assert resource["donor"] == donor["id"]
+
+    # Clean up the three new resource
+    r = client.delete("/donations/{}".format(donation["id"]))
+    assert r.status_code == 204
+    r = client.delete("/campaigns/{}".format(campaign["id"]))
+    assert r.status_code == 204
+    r = client.delete("/donors/{}".format(donor["id"]))
+    assert r.status_code == 204
---
+++ content-api/resources/methods.py
@@ -0,0 +1,129 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import json
+from google.cloud import firestore
+
+from main import request
+from data import cloud_firestore as db
+from resources import base
+
+
+resource_fields = {
+    "approvers": ["name", "email", "active"],
+    "campaigns": [
+        "name",
+        "description",
+        "cause",
+        "managers",
+        "goal",
+        "imageUrl",
+        "active",
+    ],
+    "causes": ["name", "description", "imageUrl", "active"],
+    "donations": ["campaign", "donor", "amount"],
+    "donors": ["name", "email", "mailing_address"],
+}
+
+
+def list(resource_kind):
+    if resource_kind not in resource_fields:
+        return "Not found", 404
+
+    results = db.list(resource_kind, resource_fields[resource_kind])
+    return json.dumps(results), 200, {"Content-Type": "application/json"}
+
+
+def list_subresource(resource_kind, id, subresource_kind):
+    if resource_kind not in resource_fields or subresource_kind not in resource_fields:
+        return "Not found", 404
+
+    resource = db.fetch(resource_kind, id, resource_fields[resource_kind])
+    if resource is None:
+        return "Not found", 404
+
+    results = db.list_matching(
+        subresource_kind,
+        resource_fields[subresource_kind],
+        resource_kind[:-1],  # Subresource field for singular resource_kind
+        id,  # Value must match parent id
+    )
+
+    return json.dumps(results), 200, {"Content-Type": "application/json"}
+
+
+def get(resource_kind, id):
+    if resource_kind not in resource_fields:
+        return "Not found", 404
+
+    result = db.fetch(resource_kind, id, resource_fields[resource_kind])
+    if result is None:
+        return "Not found", 404
+
+    return (
+        json.dumps(result),
+        200,
+        {"Content-Type": "application/json", "ETag": base.etag(result)},
+    )
+
+
+def insert(resource_kind, representation):
+    if resource_kind not in resource_fields:
+        return "Not found", 404
+
+    resource = db.insert(resource_kind, representation, resource_fields[resource_kind])
+
+    return (
+        json.dumps(resource),
+        201,
+        {
+            "Content-Type": "application/json",
+            "Location": resource["selfLink"],
+            "ETag": base.etag(resource),
+        },
+    )
+
+
+def patch(resource_kind, id, representation):
+    if resource_kind not in resource_fields:
+        return "Not found", 404
+
+    match_etag = request.headers.get("If-Match", None)
+    resource, status = db.update(
+        resource_kind, id, representation, resource_fields[resource_kind], match_etag
+    )
+
+    if resource is None:
+        return "", status
+
+    return (
+        json.dumps(resource),
+        201,
+        {
+            "Content-Type": "application/json",
+            "Location": resource["selfLink"],
+            "ETag": base.etag(resource),
+        },
+    )
+
+
+def delete(resource_kind, id):
+    if resource_kind not in resource_fields:
+        return "Not found", 404
+
+    match_etag = request.headers.get("If-Match", None)
+    status = db.delete(resource_kind, id, resource_fields[resource_kind], match_etag)
+
+    return "", status
