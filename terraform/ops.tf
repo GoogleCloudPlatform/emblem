d8a4999bb6afc8934bc1f69ada7f4aad1a2a34ed
Dina Graves Portman <dinagraves@google.com>
2021-07-13 11:29:34 -0700

    Spacing terraform files

--- terraform/ops.tf
+++ terraform/ops.tf
@@ -1,33 +1,33 @@
 resource "google_project" "ops_project" {
-  name       = "Emblem Ops"
-  project_id = "emblem-ops-${var.suffix}"
+  name            = "Emblem Ops"
+  project_id      = "emblem-ops-${var.suffix}"
   billing_account = var.billing_account
 }
 
 provider "google" {
-  alias = "ops"
+  alias   = "ops"
   project = google_project.ops_project.project_id
   region  = var.google_region
 }
 
 resource "google_pubsub_topic" "ops_gcr_pubsub" {
   provider = google.ops
-  name    = "gcr"
+  name     = "gcr"
 }
 
 resource "google_pubsub_topic" "ops_cloudbuilds_pubsub" {
   provider = google.ops
-  name    = "cloud-builds"
+  name     = "cloud-builds"
 }
 
 resource "google_project_service" "ops_cloudbuild_api" {
   provider = google.ops
-  service = "cloudbuild.googleapis.com"
+  service  = "cloudbuild.googleapis.com"
 }
 
 resource "google_project_service" "ops_pubsub_api" {
   provider = google.ops
-  service = "pubsub.googleapis.com"
+  service  = "pubsub.googleapis.com"
 
   # Cloud Build and many other services require Pub/Sub to be enabled. 
   # If you try to disable Pub/Sub while these services are enabled, it will fail.
@@ -44,112 +44,112 @@ provider "google-beta" {
 
 resource "google_project_service" "ops_artifact_registry_api" {
   provider = google-beta
-  service = "artifactregistry.googleapis.com"
+  service  = "artifactregistry.googleapis.com"
 }
 
 resource "google_artifact_registry_repository" "ops_website_docker" {
-  provider = google-beta
-  location = var.google_region
+  provider      = google-beta
+  location      = var.google_region
 
-  format = "DOCKER"
+  format        = "DOCKER"
   repository_id = "website"
   ## Using depends_on because the beta behavior is a little wonky
-  depends_on = [google_project_service.ops_artifact_registry_api]
+  depends_on    = [google_project_service.ops_artifact_registry_api]
 }
 
 resource "google_artifact_registry_repository" "ops_api_docker" {
-  provider = google-beta
-  location = var.google_region
+  provider      = google-beta
+  location      = var.google_region
 
-  format = "DOCKER"
+  format        = "DOCKER"
   repository_id = "content-api"
   ## Using depends_on because the beta behavior is a little wonky
-  depends_on = [google_project_service.ops_artifact_registry_api]
+  depends_on    = [google_project_service.ops_artifact_registry_api]
 }
 
 ## Give the Staging Cloud Run service account access to AR repos
 resource "google_artifact_registry_repository_iam_member" "stage_iam_api_ar" {
-  provider = google-beta
+  provider   = google-beta
 
-  location = var.google_region
+  location   = var.google_region
   repository = google_artifact_registry_repository.ops_api_docker.name
-  role   = "roles/artifactregistry.reader"
-  member = "serviceAccount:service-${google_project.stage_project.number}@serverless-robot-prod.iam.gserviceaccount.com"
+  role       = "roles/artifactregistry.reader"
+  member     = "serviceAccount:service-${google_project.stage_project.number}@serverless-robot-prod.iam.gserviceaccount.com"
   ## Using depends_on because the beta behavior is a little wonky
   depends_on = [google_artifact_registry_repository.ops_api_docker]
 }
 
 resource "google_artifact_registry_repository_iam_member" "prod_iam_api_ar" {
-  provider = google-beta
+  provider   = google-beta
 
-  location = var.google_region
+  location   = var.google_region
   repository = google_artifact_registry_repository.ops_api_docker.name
-  role   = "roles/artifactregistry.reader"
-  member = "serviceAccount:service-${google_project.prod_project.number}@serverless-robot-prod.iam.gserviceaccount.com"
+  role       = "roles/artifactregistry.reader"
+  member     = "serviceAccount:service-${google_project.prod_project.number}@serverless-robot-prod.iam.gserviceaccount.com"
   ## Using depends_on because the beta behavior is a little wonky
   depends_on = [google_artifact_registry_repository.ops_api_docker]
 }
 
 resource "google_artifact_registry_repository_iam_member" "stage_iam_website_ar" {
-  provider = google-beta
+  provider   = google-beta
 
-  location = var.google_region
+  location   = var.google_region
   repository = google_artifact_registry_repository.ops_website_docker.name
-  role   = "roles/artifactregistry.reader"
-  member = "serviceAccount:service-${google_project.stage_project.number}@serverless-robot-prod.iam.gserviceaccount.com"
+  role       = "roles/artifactregistry.reader"
+  member     = "serviceAccount:service-${google_project.stage_project.number}@serverless-robot-prod.iam.gserviceaccount.com"
   ## Using depends_on because the beta behavior is a little wonky
   depends_on = [google_artifact_registry_repository.ops_website_docker]
 }
 
 resource "google_artifact_registry_repository_iam_member" "prod_iam_website_ar" {
-  provider = google-beta
+  provider   = google-beta
 
-  location = var.google_region
+  location   = var.google_region
   repository = google_artifact_registry_repository.ops_website_docker.name
-  role   = "roles/artifactregistry.reader"
-  member = "serviceAccount:service-${google_project.prod_project.number}@serverless-robot-prod.iam.gserviceaccount.com"
+  role       = "roles/artifactregistry.reader"
+  member     = "serviceAccount:service-${google_project.prod_project.number}@serverless-robot-prod.iam.gserviceaccount.com"
   ## Using depends_on because the beta behavior is a little wonky
   depends_on = [google_artifact_registry_repository.ops_website_docker]
 }
 
 resource "google_project_iam_member" "ops_ar_admin_iam" {
   provider = google.ops
-  role   = "roles/artifactregistry.writer"
-  member = "serviceAccount:${google_project.ops_project.number}@cloudbuild.gserviceaccount.com"
+  role     = "roles/artifactregistry.writer"
+  member   = "serviceAccount:${google_project.ops_project.number}@cloudbuild.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "ops_cloudbuild_service_account_user_iam_stage" {
   provider = google.stage
-  role   = "roles/iam.serviceAccountUser"
-  member = "serviceAccount:${google_project.ops_project.number}@cloudbuild.gserviceaccount.com"
+  role     = "roles/iam.serviceAccountUser"
+  member   = "serviceAccount:${google_project.ops_project.number}@cloudbuild.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "ops_cloudbuild_run_admin_iam_stage" {
   provider = google.stage
-  role   = "roles/run.admin"
-  member = "serviceAccount:${google_project.ops_project.number}@cloudbuild.gserviceaccount.com"
+  role     = "roles/run.admin"
+  member   = "serviceAccount:${google_project.ops_project.number}@cloudbuild.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "ops_cloudbuild_pubsub_iam_stage" {
   provider = google.stage
-  role   = "roles/pubsub.publisher"
-  member = "serviceAccount:${google_project.ops_project.number}@cloudbuild.gserviceaccount.com"
+  role     = "roles/pubsub.publisher"
+  member   = "serviceAccount:${google_project.ops_project.number}@cloudbuild.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "ops_cloudbuild_service_account_user_iam_prod" {
   provider = google.prod
-  role   = "roles/iam.serviceAccountUser"
-  member = "serviceAccount:${google_project.ops_project.number}@cloudbuild.gserviceaccount.com"
+  role     = "roles/iam.serviceAccountUser"
+  member   = "serviceAccount:${google_project.ops_project.number}@cloudbuild.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "ops_cloudbuild_run_admin_iam_prod" {
   provider = google.prod
-  role   = "roles/run.admin"
-  member = "serviceAccount:${google_project.ops_project.number}@cloudbuild.gserviceaccount.com"
+  role     = "roles/run.admin"
+  member   = "serviceAccount:${google_project.ops_project.number}@cloudbuild.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "ops_cloudbuild_pubsub_iam_prod" {
   provider = google.prod
-  role   = "roles/pubsub.publisher"
-  member = "serviceAccount:${google_project.ops_project.number}@cloudbuild.gserviceaccount.com"
+  role     = "roles/pubsub.publisher"
+  member   = "serviceAccount:${google_project.ops_project.number}@cloudbuild.gserviceaccount.com"
 }
--- terraform/prod.tf
+++ terraform/prod.tf
@@ -1,12 +1,12 @@
 provider "google" {
-  alias = "prod"
+  alias   = "prod"
   project = google_project.prod_project.project_id
   region  = var.google_region
 }
 
 resource "google_project" "prod_project" {
-  name       = "Emblem Prod"
-  project_id = "emblem-prod-${var.suffix}"
+  name            = "Emblem Prod"
+  project_id      = "emblem-prod-${var.suffix}"
   billing_account = var.billing_account
 }
 
@@ -14,44 +14,44 @@ resource "google_service_account" "prod_cloud_run_manager" {
   account_id   = "cloud-run-manager"
   description  = "Account for deploying new revisions and controlling traffic to Cloud Run"
   display_name = "cloud-run-manager"
-  provider = google.prod
+  provider     = google.prod
 }
 
 resource "google_pubsub_topic" "prod_canary_pubsub" {
-  name    = "canary"
+  name     = "canary"
   provider = google.prod
 }
 
 resource "google_pubsub_topic" "prod_cloudbuilds_pubsub" {
-  name    = "cloud-builds"
+  name     = "cloud-builds"
   provider = google.prod
 }
 
 resource "google_project_service" "prod_cloudbuild_api" {
   provider = google.prod
-  service = "cloudbuild.googleapis.com"
+  service  = "cloudbuild.googleapis.com"
 }
 
 resource "google_project_service" "prod_run_api" {
   provider = google.prod
-  service = "run.googleapis.com"
+  service  = "run.googleapis.com"
 }
 
 resource "google_project_service" "prod_pubsub_api" {
-  provider = google.prod
-  service = "pubsub.googleapis.com"
+  provider                   = google.prod
+  service                    = "pubsub.googleapis.com"
   disable_dependent_services = true
 }
 
 resource "google_project_iam_member" "prod_cloudbuild_service_account_user_iam" {
   provider = google.prod
-  role   = "roles/iam.serviceAccountUser"
-  member = "serviceAccount:${google_project.prod_project.number}@cloudbuild.gserviceaccount.com"
+  role     = "roles/iam.serviceAccountUser"
+  member   = "serviceAccount:${google_project.prod_project.number}@cloudbuild.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "prod_cloudbuild_run_admin_iam" {
   provider = google.prod
-  role   = "roles/run.admin"
-  member = "serviceAccount:${google_project.prod_project.number}@cloudbuild.gserviceaccount.com"
+  role     = "roles/run.admin"
+  member   = "serviceAccount:${google_project.prod_project.number}@cloudbuild.gserviceaccount.com"
 }
 
