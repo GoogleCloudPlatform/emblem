f58bb17b5fcd53515ea4076bd668b9b00bdf58b3
ace-n <anassri@google.com>
2021-08-02 14:26:05 -0700

    Use icons instead of avatars

--- website/views/auth.py
+++ website/views/auth.py
@@ -13,20 +13,57 @@
 # limitations under the License.
 
 
+import datetime
 import os
 
-from flask import Blueprint, render_template
+import firebase_admin
+
+from firebase_admin import auth, exceptions
+from flask import abort, Blueprint, make_response, redirect, request, render_template
 
 
 auth_bp = Blueprint("auth", __name__, template_folder="templates")
+default_app = firebase_admin.initialize_app()
+
+
+@auth_bp.route("/login", methods=["POST"])
+def login_post():
+    oauth_client_id = os.getenv('OAUTH_CLIENT_ID');
+
+    id_token = request.form['idToken']
+    expires_in = datetime.timedelta(days=5)
+
+    try:
+        # Validate ID token + create session cookie
+        session_cookie = auth.create_session_cookie(
+            id_token, expires_in=expires_in)
+
+        # Configure response to store session cookie
+        expires = datetime.datetime.now() + expires_in
+        response = make_response(redirect('/'))
+        response.set_cookie(
+            'session',
+            session_cookie,
+            expires=expires,
+            httponly=True,
+            secure=True
+        )
+
+        return response
+    except exceptions.FirebaseError:
+        return flask.abort(401, 'Failed to create a session cookie')
 
 
 @auth_bp.route("/login", methods=["GET"])
-def login():
-    oauth_client_id = os.getenv('OAUTH_CLIENT_ID');
-    return render_template("auth/login.html", OAUTH_CLIENT_ID=oauth_client_id)
+def login_get():
+    return render_template("auth/login.html")
 
 
 @auth_bp.route("/logout", methods=["GET"])
 def logout():
-    return render_template("auth/logout.html")
+    response = make_response(redirect('/'))
+
+    # Clear session token
+    response.set_cookie('session', '', expires=0)
+
+    return response
