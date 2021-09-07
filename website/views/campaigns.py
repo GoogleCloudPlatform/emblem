99e30d8652f18128a3d36931ffaf24ed925d2c73
Charles Engelke <engelke@google.com>
2021-09-07 15:47:52 -0700

    Website campains operations use client libraries

--- website/views/campaigns.py
+++ website/views/campaigns.py
@@ -12,11 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from sample_data import SAMPLE_DONATIONS
-
 from flask import Blueprint, redirect, request, render_template
 
+import os
 import requests
+
+import emblem_api
+
 
 campaigns_bp = Blueprint("campaigns", __name__, template_folder="templates")
 
@@ -25,7 +27,8 @@ API_URL = "https://api-pwrmtjf4hq-uc.a.run.app"
 
 @campaigns_bp.route("/")
 def list_campaigns():
-    campaigns = requests.get(API_URL + "/campaigns").json()
+    api = emblem_api.EmblemApi(os.environ.get("API_URL", None))
+    campaigns = api.campaigns_get()
     return render_template("home.html", campaigns=campaigns)
 
 
@@ -36,20 +39,34 @@ def new_campaign():
 
 @campaigns_bp.route("/createCampaign", methods=["POST"])
 def save_campaign():
+    access_key = None
+    auth_header = request.header.get("Authorization", None)
+    if auth_header is not None:
+        access_key = auth_header[:7]
+    api = emblem_api.EmblemApi(os.environ.get("API_URL", None), access_key=access_key)
     # TODO: do something with the collected data
-    print("Name: ", request.form["name"])
-    print("Goal: ", request.form["goal"])
-    print("Managers: ", request.form["managers"])
+    api.campaigns_post({
+        "name": request.form["name"],
+        "goal": request.form["goal"],
+        "managers": request.form["managers"]
+    })
 
     return redirect("/")
 
 
 @campaigns_bp.route("/viewCampaign")
 def webapp_view_campaign():
-    campaigns = requests.get(API_URL + "/campaigns").json()
+    access_key = None
+    auth_header = request.header.get("Authorization", None)
+    if auth_header is not None:
+        access_key = auth_header[:7]
+    api = emblem_api.EmblemApi(os.environ.get("API_URL", None), access_key=access_key)
+
+    campaigns = api.campaigns_get()
     campaign_instance = campaigns[0]
 
     # Add dummy data for donations
-    campaign_instance["donations"] = SAMPLE_DONATIONS
+    campaign_instance["donations"] = []
+    campaign_instance["donations"] = api.campaigns_id_donations_get(campaign_instance["id"])
 
     return render_template("view-campaign.html", campaign=campaign_instance)
