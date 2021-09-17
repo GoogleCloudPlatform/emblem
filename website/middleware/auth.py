efcf8b617a2d5cf8be3aa4de205fe0c2e96a751b
ace-n <anassri@google.com>
2021-09-17 11:55:01 -0700

    Address comments

--- website/middleware/auth.py
+++ website/middleware/auth.py
@@ -25,5 +25,5 @@ def init(app):
         id_token = request.cookies.get("session", None)
 
         g.api = emblem_client.EmblemClient(
-            os.environ.get("API_URL", None), access_token=id_token
+            app.config.get("API_URL", None), access_token=id_token
         )
