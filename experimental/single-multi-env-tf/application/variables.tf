a96b4b034a6632996151b60c1c5e5069a71836a0
Adam Ross <adamross@google.com>
2021-11-30 15:41:51 -0800

    experimental(delivery): implement application module (#256)
    Co-authored-by: Dina Graves Portman <dinagraves@google.com>

--- experimental/single-multi-env-tf/application/variables.tf
+++ experimental/single-multi-env-tf/application/variables.tf
@@ -5,3 +5,14 @@ variable "google_project_id" {
 variable "environment" {
   type = string
 }
+
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
+
+# Sets the time-to-live in days for user sessions.
+variable "session_bucket_ttl" {
+  type    = number
+  default = 14
+}
