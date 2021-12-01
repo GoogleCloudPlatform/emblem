96b0208905ede9a6a81294c4bc89d70c6ba9b49b
Patti Shin <pattishin@google.com>
2021-12-01 11:34:35 -0800

    Add terraform configs for website-unit-tests web-push-to-main

--- terraform/cloud_build_triggers.tf
+++ terraform/cloud_build_triggers.tf
@@ -28,3 +28,39 @@ resource "google_cloudbuild_trigger" "api_push_to_main_build_trigger" {
     }
   }
 }
+
+resource "google_cloudbuild_trigger" "website_unit_tests_build_trigger" {
+  name = "website-unit-tests"
+  filename = "ops/unit-tests.cloudbuild.yaml"
+  included_files = ["website/**"]
+  substitutions = {
+    _DIR = "website"
+  }
+  github {
+    owner = "GoogleCloudPlatform"
+    name = "emblem"
+    pull_request {
+      branch = "^main$"
+      comment_control="COMMENTS_ENABLED_FOR_EXTERNAL_CONTRIBUTORS_ONLY"
+    }
+  }
+}
+
+resource "google_cloudbuild_trigger" "web_push_to_main_build_trigger" {
+  name = "web-push-to-main"
+  filename = "ops/web-build.cloudbuild.yaml"
+  included_files = [
+    "website/*",
+    "website/*/*",
+    "client-libs/python/*"
+  ]
+  github {
+    owner = "GoogleCloudPlatform"
+    name = "emblem"
+    push {
+      branch = "^main$"
+    }
+  }
+}
+
+
