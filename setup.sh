f32560d678bfe20c8c00a846d4f2efd524f896aa
ace-n <anassri@google.com>
2021-12-06 10:10:27 -0800

    feat(setup.sh): add OAuth setup flow

--- setup.sh
+++ setup.sh
@@ -103,6 +103,87 @@ gcloud run deploy --allow-unauthenticated \
 --set-env-vars "$WEBSITE_VARS" \
 website
 
+###############
+# Set up auth #
+###############
+
+PROD_WEBSITE_URL=$(gcloud run services list --project ${PROD_PROJECT} --format "value(URL)" | grep website)
+STAGE_WEBSITE_URL=$(gcloud run services list --project ${STAGE_PROJECT} --format "value(URL)" | grep website)
+
+PROD_CALLBACK_URL="${PROD_WEBSITE_URL}/callback"
+STAGE_CALLBACK_URL="${STAGE_WEBSITE_URL}/callback"
+
+AUTH_CLIENT_CREATION_URL="https://console.cloud.google.com/apis/credentials/oauthclient?project=${OPS_PROJECT}"
+AUTH_CLIENT_CONSENT_SCREEN_URL="https://console.cloud.google.com/apis/credentials/consent?project=${OPS_PROJECT}"
+
+echo ""
+read -p "Would you like to configure $(tput bold)$(tput setaf 3)end-user authentication?$(tput sgr0) (y/n) " auth_yesno
+
+if [[ ${auth_yesno} == "y" ]]; then
+    # Configure consent screen
+    echo "--------------------------------------------"
+    echo "$(tput setaf 6)Configure OAuth 2.0 consent screen$(tput sgr0)"
+    echo ""
+    echo "  Visit this URL in the Cloud Console: $(tput bold)${AUTH_CLIENT_CONSENT_SCREEN_URL}$(tput sgr0)"
+    echo ""
+    echo "  Create an $(tput bold)External$(tput sgr0) application, and:"
+    echo "   - use the $(tput bold)Default Scopes$(tput sgr0)"
+    echo "   - add yourself as a $(tput bold)Test User$(tput sgr0)"
+    echo ""
+    echo "  Otherwise, keep the default settings."
+    echo ""
+    open ${AUTH_CLIENT_CREATION_URL}
+    read -p "Once you've configured your consent screen, press $(tput bold)Enter$(tput sgr0) to continue."
+
+    # Create OAuth client
+    echo "--------------------------------------------"
+    echo "$(tput setaf 6)Create an OAuth 2.0 client$(tput sgr0)"
+    echo ""
+    echo "  Visit this URL in the Cloud Console: $(tput bold)${AUTH_CLIENT_CREATION_URL}$(tput sgr0)"
+    echo ""
+    echo "  For $(tput bold)Application Type$(tput sgr0), select $(tput bold)Web Application$(tput sgr0)."
+    echo "  Under $(tput bold)Authorized Redirect URIs$(tput sgr0), add the following URLs:"
+    echo ""
+    echo "    ${PROD_CALLBACK_URL}"
+    echo "    ${STAGE_CALLBACK_URL}"
+    echo ""
+    echo "  Then, click $(tput bold)Create$(tput sgr0)."
+    echo ""
+    echo "  $(tput bold)Keep the resulting pop-up open!$(tput sgr0) You'll need those values in the next step."
+    echo ""
+    open ${AUTH_CLIENT_CREATION_URL}
+    read -p "Once you've configured an OAuth client, press $(tput bold)Enter$(tput sgr0) to continue."
+
+    # Prompt user to create secret versions
+    SECRETS_URL="https://console.cloud.google.com/security/secret-manager?project=${OPS_PROJECT}"
+    echo "--------------------------------------------"
+    echo "$(tput setaf 6)Configure secret values$(tput sgr0)"
+    echo ""
+    echo "  Visit this URL in the Cloud Console: $(tput bold)${SECRETS_URL}$(tput sgr0)"
+    echo ""
+    echo "  Add new secret versions to the existing secrets. Set $(tput bold)secret value$(tput sgr0)"
+    echo "  to the credential values displayed by the $(tput bold)previous step's pop-up$(tput sgr0)."
+    echo ""
+    open ${SECRETS_URL}
+    read -p "Once you've configured secret versions, press $(tput bold)Enter$(tput sgr0) to continue."
+
+    # Update website Cloud Run services with required secrets
+    OPS_PROJECT_NUMBER=$(gcloud projects describe ${OPS_PROJECT} --format "value(projectNumber)")
+
+    AUTH_SECRETS="CLIENT_ID=projects/${OPS_PROJECT_NUMBER}/secrets/client-id-secret:latest"
+    AUTH_SECRETS="${AUTH_SECRETS},CLIENT_SECRET=projects/${OPS_PROJECT_NUMBER}/secrets/client-secret-secret:latest"
+
+    # TODO: do we want to keep the redirect URI secret, or set it as an env var?
+
+    gcloud run services update website \
+        --update-env-vars "REDIRECT_URI=${STAGE_CALLBACK_URL}" \
+        --set-secrets "${AUTH_SECRETS}" \
+        --project "$STAGE_PROJECT"
+    gcloud run services update website \
+        --update-env-vars "REDIRECT_URI=${PROD_CALLBACK_URL}" \
+        --set-secrets "${AUTH_SECRETS}" \
+        --project "$PROD_PROJECT"
+fi
 
 ################
 # Set up CI/CD #
