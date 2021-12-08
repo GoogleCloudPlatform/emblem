efde0487f848c11167e8cb3bc1b681f783d2f5eb
Dina Graves Portman <dinagraves@google.com>
2021-12-08 13:45:15 -0500

    Trying to make the tf linter bot happy

--- terraform/ops/triggers/cloud_build_triggers.tf
+++ terraform/ops/triggers/cloud_build_triggers.tf
@@ -1,6 +1,6 @@
 resource "google_cloudbuild_trigger" "api_unit_tests_build_trigger" {
-  project = var.google_ops_project_id
-  name = "api-unit-tests"
+  project  = var.google_ops_project_id
+  name     = "api-unit-tests"
   filename = "ops/unit-tests.cloudbuild.yaml"
   included_files = ["content-api/**"]
    substitutions = {
@@ -9,7 +9,7 @@ resource "google_cloudbuild_trigger" "api_unit_tests_build_trigger" {
    }
    github {
      owner = var.repo_owner
-     name = var.repo_name
+     name  = var.repo_name
      pull_request {
        branch = "^main$"
        comment_control="COMMENTS_ENABLED_FOR_EXTERNAL_CONTRIBUTORS_ONLY"
@@ -18,52 +18,52 @@ resource "google_cloudbuild_trigger" "api_unit_tests_build_trigger" {
  }
 
 resource "google_cloudbuild_trigger" "api_push_to_main_build_trigger" {
-   project = var.google_ops_project_id
-   name = "api-push-to-main"
-   filename = "ops/api-build.cloudbuild.yaml"
-   included_files = ["content-api/**"]
-    github {
-      owner = var.repo_owner
-      name = var.repo_name
-      push {
-        branch = "^main$"
-      }
+  project = var.google_ops_project_id
+  name = "api-push-to-main"
+  filename = "ops/api-build.cloudbuild.yaml"
+  included_files = ["content-api/**"]
+  github {
+    owner = var.repo_owner
+    name = var.repo_name
+    push {
+      branch = "^main$"
     }
   }
+}
 
 resource "google_cloudbuild_trigger" "website_unit_tests_build_trigger" {
-    project = var.google_ops_project_id
-    name = "website-unit-tests"
-    filename = "ops/unit-tests.cloudbuild.yaml"
-    included_files = ["website/**"]
-     substitutions = {
-       _DIR = "website"
-     }
-     github {
-       owner = var.repo_owner
-       name = var.repo_name
-       pull_request {
-         branch = "^main$"
-         comment_control="COMMENTS_ENABLED_FOR_EXTERNAL_CONTRIBUTORS_ONLY"
-       }
-     }
-   }
+  project = var.google_ops_project_id
+  name = "website-unit-tests"
+  filename = "ops/unit-tests.cloudbuild.yaml"
+  included_files = ["website/**"]
+  substitutions = {
+    _DIR = "website"
+  }
+  github {
+    owner = var.repo_owner
+    name = var.repo_name
+    pull_request {
+    branch = "^main$"
+    comment_control="COMMENTS_ENABLED_FOR_EXTERNAL_CONTRIBUTORS_ONLY"
+    }
+  }
+}
 
 resource "google_cloudbuild_trigger" "web_push_to_main_build_trigger" {
-     project = var.google_ops_project_id
-     name = "web-push-to-main"
-     filename = "ops/web-build.cloudbuild.yaml"
-     included_files = [
-       "website/*",
-        "website/*/*",
-        "client-libs/python/*"
-       ]
-       github {
-         owner = var.repo_owner
-         name = var.repo_name
-         push {
-           branch = "^main$"
-         }
-       }
-     }
+  project = var.google_ops_project_id
+  name = "web-push-to-main"
+  filename = "ops/web-build.cloudbuild.yaml"
+  included_files = [
+    "website/*",
+    "website/*/*",
+    "client-libs/python/*"
+    ]
+  github {
+    owner = var.repo_owner
+    name = var.repo_name
+    push {
+      branch = "^main$"
+      }
+    }
+}
 
