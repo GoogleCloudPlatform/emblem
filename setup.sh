dbd9af7a2ddf7f44a90776a99836485f28c550a2
Dina Graves Portman <dinagraves@google.com>
2022-01-24 15:59:50 -0500

    Suppress error message for Staging as well

--- setup.sh
+++ setup.sh
@@ -63,7 +63,7 @@ terraform init --backend-config "path=./stage.tfstate" -reconfigure
 # Note: If AppEngine is in a different region than Cloud Run or in the wrong mode 
 # (Datastore vs Firestore), this could cause latency or query compatibility issues.
 
-terraform import module.application.google_app_engine_application.main "${STAGE_PROJECT}" || true
+terraform import module.application.google_app_engine_application.main "${STAGE_PROJECT}" 2>/dev/null || true
 terraform apply --auto-approve 
 
 # Firestore requires App Engine for automatic provisioning.
