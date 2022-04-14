a667b4e9bb4822a7057fda8f0228afe706e8d38e
rogerthatdev <roger.that.dev@gmail.com>
2022-04-13 21:58:57 -0700

    sample files for ops project

---
+++ experimental/conventional-model/terraform/environments/ops/variables.tf
@@ -0,0 +1,17 @@
+variables "project_id" {
+    type    = string
+    description = ""
+}
+variables "region" {
+    type    = string
+    default = "us-central1"
+    description = ""
+}
+variables "repo_owner" {
+    type    = string
+    description = ""
+}
+variables "repo_name" {
+    type    = string
+    description = ""
+}
