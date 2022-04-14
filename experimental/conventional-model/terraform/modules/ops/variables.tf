c704518f45d05f1b627051c4fc3e3bb34801a877
rogerthatdev <roger.that.dev@gmail.com>
2022-04-13 17:35:33 -0700

    ops module files

---
+++ experimental/conventional-model/terraform/modules/ops/variables.tf
@@ -0,0 +1,21 @@
+# Project IDs
+variable "project_id" {
+  description = "Google Cloud Project to deploy module resources."
+  type        = string
+}
+
+variable "region" {
+  type        = string
+  default     = "us-central1"
+  description = "Google Cloud Region"
+}
+
+variable "repo_owner" {
+  description = "Repo Owner (GoogleCloudPlatform)"
+  type        = string
+}
+
+variable "repo_name" {
+  description = "Repo Name (emblem)"
+  type        = string
+}
