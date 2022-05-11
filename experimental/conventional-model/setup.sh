3c80cc864a0cd9c1d69c6f38805bfb62f1782232
Roger Martinez <rogerthatdev@gmail.com>
2022-05-11 16:12:26 -0700

    deploys initial ops

--- experimental/conventional-model/setup.sh
+++ experimental/conventional-model/setup.sh
@@ -3,14 +3,17 @@ set -eu
 # Required Environment variables:
 # OPS_PROJECT_ID
 # STAGING_PROJECT_ID
+# REPO_NAME
+# REPO_OWNER
 
 ## Ops Project (minus triggers) ##
 
 OPS_ENVIRONMENT_DIR=terraform/environments/ops
 export TF_VAR_project_id=${OPS_PROJECT_ID}
 terraform -chdir=${OPS_ENVIRONMENT_DIR} init
-terraform -chdir=${OPS_ENVIRONMENT_DIR} plan
+terraform -chdir=${OPS_ENVIRONMENT_DIR} apply
 
+exit
 ## Staging Project ##
 
 STAGING_ENVIRONMENT_DIR=terraform/environments/staging
@@ -18,7 +21,7 @@ export TF_VAR_project_id=${STAGING_PROJECT_ID}
 export TF_VAR_ops_project_id=${OPS_PROJECT_ID}
 
 terraform -chdir=${STAGING_ENVIRONMENT_DIR} init  
-terraform -chdir=${STAGING_ENVIRONMENT_DIR} Plan
+terraform -chdir=${STAGING_ENVIRONMENT_DIR} apply
 
 ## Prod Project ##
 
@@ -27,10 +30,30 @@ export TF_VAR_project_id=${PROD_PROJECT_ID}
 export TF_VAR_ops_project_id=${OPS_PROJECT_ID}
 
 terraform -chdir=${PROD_ENVIRONMENT_DIR} init  
-terraform -chdir=${PROD_ENVIRONMENT_DIR} Plan
+terraform -chdir=${PROD_ENVIRONMENT_DIR} apply
+
+# ## Build Containers ##
+
+# export REGION="us-central1"
+# SHORT_SHA="setup"
+# E2E_RUNNER_TAG="latest"
+
+# gcloud builds submit --config=ops/api-build.cloudbuild.yaml \
+# --project="$OPS_PROJECT" --substitutions=_REGION="$REGION",SHORT_SHA="$SHORT_SHA"
+
+# gcloud builds submit --config=ops/web-build.cloudbuild.yaml \
+# --project="$OPS_PROJECT" --substitutions=_REGION="$REGION",SHORT_SHA="$SHORT_SHA"
+
+# gcloud builds submit --config=ops/e2e-runner-build.cloudbuild.yaml \
+# --project="$OPS_PROJECT" --substitutions=_REGION="$REGION",_IMAGE_TAG="$E2E_RUNNER_TAG"
+
+
+
 
 # DEPLOY TRIGGERS TO OPS PROJECT
 
 export TF_VAR_project_id=${OPS_PROJECT}
 export TF_VAR_deploy_triggers="true"
-terraform -chdir=${OPS_ENVIRONMENT_DIR} plan // Plan: 4 to add
+export TF_VAR_repo_name=${REPO_NAME}
+export TF_VAR_repo_owner=${REPO_OWNER}
+terraform -chdir=${OPS_ENVIRONMENT_DIR} plan
