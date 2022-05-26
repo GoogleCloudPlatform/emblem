7d3c2628b6ed12b379ede9e0dae6849727088a90
ace-n <anassri@google.com>
2022-05-26 16:48:05 -0700

    Avoid unnecessary changes

--- terraform/cleanup.sh
+++ terraform/cleanup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2022 Google LLC
+# Copyright 2021 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -118,5 +118,5 @@ done
 popd
 
 echo "###################################################"
-echo "#               End clean-up script               #"
+echo "#                   End cleanup                   #"
 echo "###################################################"
