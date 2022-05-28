ea0516d5eccd6859e13ea1eac2d09888889ba8f4
Roger Martinez <rogerthatdev@gmail.com>
2022-05-27 18:00:42 -0700

    style: bash format

--- experimental/conventional-model/setup.sh
+++ experimental/conventional-model/setup.sh
@@ -34,7 +34,7 @@ STAGING_ENVIRONMENT_DIR=terraform/environments/staging
 export TF_VAR_project_id=${STAGING_PROJECT_ID}
 export TF_VAR_ops_project_id=${OPS_PROJECT_ID}
 
-terraform -chdir=${STAGING_ENVIRONMENT_DIR} init  
+terraform -chdir=${STAGING_ENVIRONMENT_DIR} init
 terraform -chdir=${STAGING_ENVIRONMENT_DIR} apply
 
 ## Prod Project ##
@@ -43,7 +43,7 @@ PROD_ENVIRONMENT_DIR=terraform/environments/prod
 export TF_VAR_project_id=${PROD_PROJECT_ID}
 export TF_VAR_ops_project_id=${OPS_PROJECT_ID}
 
-terraform -chdir=${PROD_ENVIRONMENT_DIR} init  
+terraform -chdir=${PROD_ENVIRONMENT_DIR} init
 terraform -chdir=${PROD_ENVIRONMENT_DIR} apply
 
 ## Build Containers ##
@@ -54,39 +54,39 @@ SHORT_SHA="setup"
 E2E_RUNNER_TAG="latest"
 
 gcloud builds submit --config=ops/api-build.cloudbuild.yaml \
---project="$OPS_PROJECT_ID" --substitutions=_REGION="$REGION",SHORT_SHA="$SHORT_SHA"
+    --project="$OPS_PROJECT_ID" --substitutions=_REGION="$REGION",SHORT_SHA="$SHORT_SHA"
 
 gcloud builds submit --config=ops/web-build.cloudbuild.yaml \
---project="$OPS_PROJECT_ID" --substitutions=_REGION="$REGION",SHORT_SHA="$SHORT_SHA"
+    --project="$OPS_PROJECT_ID" --substitutions=_REGION="$REGION",SHORT_SHA="$SHORT_SHA"
 
 gcloud builds submit --config=ops/e2e-runner-build.cloudbuild.yaml \
---project="$OPS_PROJECT_ID" --substitutions=_REGION="$REGION",_IMAGE_TAG="$E2E_RUNNER_TAG"
+    --project="$OPS_PROJECT_ID" --substitutions=_REGION="$REGION",_IMAGE_TAG="$E2E_RUNNER_TAG"
 
 cd -
 
 ## Prod Services ##
 
 gcloud run deploy --allow-unauthenticated \
---image "${REGION}-docker.pkg.dev/${OPS_PROJECT_ID}/content-api/content-api:${SHORT_SHA}" \
---project "$PROD_PROJECT_ID"  --service-account "api-manager@${PROD_PROJECT_ID}.iam.gserviceaccount.com" \
---region "${REGION}" content-api
+    --image "${REGION}-docker.pkg.dev/${OPS_PROJECT_ID}/content-api/content-api:${SHORT_SHA}" \
+    --project "$PROD_PROJECT_ID" --service-account "api-manager@${PROD_PROJECT_ID}.iam.gserviceaccount.com" \
+    --region "${REGION}" content-api
 
 PROD_API_URL=$(gcloud run services describe content-api --project ${PROD_PROJECT_ID} --region ${REGION} --format "value(status.url)")
 WEBSITE_VARS="EMBLEM_SESSION_BUCKET=${PROD_PROJECT_ID}-sessions"
 WEBSITE_VARS="${WEBSITE_VARS},EMBLEM_API_URL=${PROD_API_URL}"
 
 gcloud run deploy --allow-unauthenticated \
---image "${REGION}-docker.pkg.dev/${OPS_PROJECT_ID}/website/website:${SHORT_SHA}" \
---project "$PROD_PROJECT_ID" --service-account "website-manager@${PROD_PROJECT_ID}.iam.gserviceaccount.com"  \
---set-env-vars "$WEBSITE_VARS" --region "${REGION}" --tag "latest" \
-website
+    --image "${REGION}-docker.pkg.dev/${OPS_PROJECT_ID}/website/website:${SHORT_SHA}" \
+    --project "$PROD_PROJECT_ID" --service-account "website-manager@${PROD_PROJECT_ID}.iam.gserviceaccount.com" \
+    --set-env-vars "$WEBSITE_VARS" --region "${REGION}" --tag "latest" \
+    website
 
 ## Staging Services ##
 
 gcloud run deploy --allow-unauthenticated \
---image "${REGION}-docker.pkg.dev/${OPS_PROJECT_ID}/content-api/content-api:${SHORT_SHA}" \
---project "$STAGING_PROJECT_ID"  --service-account "api-manager@${STAGING_PROJECT_ID}.iam.gserviceaccount.com"  \
---region "${REGION}" content-api
+    --image "${REGION}-docker.pkg.dev/${OPS_PROJECT_ID}/content-api/content-api:${SHORT_SHA}" \
+    --project "$STAGING_PROJECT_ID" --service-account "api-manager@${STAGING_PROJECT_ID}.iam.gserviceaccount.com" \
+    --region "${REGION}" content-api
 
 STAGING_API_URL=$(gcloud run services describe content-api --project ${STAGING_PROJECT_ID} --region ${REGION} --format "value(status.url)")
 
@@ -94,20 +94,19 @@ WEBSITE_VARS="EMBLEM_SESSION_BUCKET=${STAGING_PROJECT_ID}-sessions"
 WEBSITE_VARS="${WEBSITE_VARS},EMBLEM_API_URL=${STAGING_API_URL}"
 
 gcloud run deploy --allow-unauthenticated \
---image "${REGION}-docker.pkg.dev/${OPS_PROJECT_ID}/website/website:${SHORT_SHA}" \
---project "$STAGING_PROJECT_ID" --service-account "website-manager@${STAGING_PROJECT_ID}.iam.gserviceaccount.com" \
---set-env-vars "$WEBSITE_VARS" --region "${REGION}" --tag "latest" \
-website
+    --image "${REGION}-docker.pkg.dev/${OPS_PROJECT_ID}/website/website:${SHORT_SHA}" \
+    --project "$STAGING_PROJECT_ID" --service-account "website-manager@${STAGING_PROJECT_ID}.iam.gserviceaccount.com" \
+    --set-env-vars "$WEBSITE_VARS" --region "${REGION}" --tag "latest" \
+    website
 
 ## Deploy Triggers to Ops Project ##
 
 if [[ -z "$SKIP_TRIGGERS" ]]; then
-    REPO_CONNECT_URL="https://console.cloud.google.com/cloud-build/triggers/connect?\project=${OPS_PROJECT_ID}"
+    REPO_CONNECT_URL="https://console.cloud.google.com/cloud-build/triggers/connect?project=${OPS_PROJECT_ID}"
     echo "Connect your repos: ${REPO_CONNECT_URL}"
     read -p "Once your repo is connected, please continue by typing any key."
     continue=1
-    while [[ ${continue} -gt 0 ]]
-    do
+    while [[ ${continue} -gt 0 ]]; do
 
         read -p "Please input the repo owner [GoogleCloudPlatform]: " REPO_OWNER
         repo_owner=${repo_owner:-GoogleCloudPlatform}
@@ -116,8 +115,8 @@ if [[ -z "$SKIP_TRIGGERS" ]]; then
 
         read -p "Is this the correct repo: ${REPO_OWNER}/${REPO_NAME}? (y/n) " yesno
 
-        if [[ ${yesno} == "y" ]]
-            then continue=0
+        if [[ ${yesno} == "y" ]]; then
+            continue=0
         fi
 
     done
