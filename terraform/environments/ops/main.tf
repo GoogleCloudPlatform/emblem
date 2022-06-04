ebd69d12ebeb2d7001aa8afc0122340a72142664
Adam Ross <adamross@google.com>
2022-06-04 15:07:53 -0700

    fix: fmt error in ops/main.tf

--- terraform/environments/ops/main.tf
+++ terraform/environments/ops/main.tf
@@ -1,8 +1,8 @@
 module "emblem_ops" {
-  source                  = "../../modules/ops"
-  project_id              = var.project_id
-  region                  = var.region
-  deploy_triggers         = var.deploy_triggers
-  repo_owner              = var.repo_owner
-  repo_name               = var.repo_name
+  source          = "../../modules/ops"
+  project_id      = var.project_id
+  region          = var.region
+  deploy_triggers = var.deploy_triggers
+  repo_owner      = var.repo_owner
+  repo_name       = var.repo_name
 }
