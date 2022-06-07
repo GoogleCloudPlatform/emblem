1649cda98e0806f053ca3f7b24935abcf985d28a
Ace Nassri <anassri@google.com>
2022-06-07 13:35:38 -0700

    Env var fixes

--- installation-testing/reusable-project-setup.sh
+++ installation-testing/reusable-project-setup.sh
@@ -25,7 +25,6 @@ trap '_error_report $LINENO' ERR
 # Variable list
 #   PROD_PROJECT            GCP Project ID of the production project
 #   STAGE_PROJECT           GCP Project ID of the staging project
-#   SKIP_TERRAFORM          If set, don't set up infrastructure
 
 # Default to empty, avoiding unbound variable errors.
 SKIP_TERRAFORM=${SKIP_TERRAFORM:-}
@@ -37,31 +36,28 @@ if [[ -z "${PROD_PROJECT}" ]]; then
 elif [[ -z "${STAGE_PROJECT}" ]]; then
     echo "Please set the $(tput bold)STAGE_PROJECT$(tput sgr0) variable"
     exit 1
+elif [[ -z "${OPS_PROJECT}" ]]; then
+    echo "Please set the $(tput bold)OPS_PROJECT$(tput sgr0) variable"
+    exit 1
 fi
 
-## Initialize Variables ##
-export REGION="us-central1"
+## Staging Project ##
+STAGE_ENVIRONMENT_DIR=terraform/environments/staging
+export TF_VAR_project_id=${STAGE_PROJECT}
+export TF_VAR_ops_project_id=${OPS_PROJECT}
+terraform -chdir=${STAGE_ENVIRONMENT_DIR} init
+terraform -chdir=${STAGE_ENVIRONMENT_DIR} import \
+    module.emblem_staging.google_storage_bucket.sessions \
+    "${STAGE_PROJECT}-sessions" || true
 
-if [[ -z "$SKIP_TERRAFORM" ]]; then
-
-    ## Staging Project ##
-    STAGE_ENVIRONMENT_DIR=terraform/environments/staging
-    export TF_VAR_project_id=${STAGE_PROJECT}
+## Prod Project ##
+# Only deploy to separate project for multi-project setups
+if [ "${PROD_PROJECT}" != "${STAGE_PROJECT}" ]; then 
+    PROD_ENVIRONMENT_DIR=terraform/environments/prod
+    export TF_VAR_project_id=${PROD_PROJECT}
     export TF_VAR_ops_project_id=${OPS_PROJECT}
-    terraform -chdir=${STAGE_ENVIRONMENT_DIR} init
-    terraform -chdir=${STAGE_ENVIRONMENT_DIR} import \
-        module.emblem_staging.google_storage_bucket.sessions \
-        "${STAGE_PROJECT}-sessions" || true
-
-    ## Prod Project ##
-    # Only deploy to separate project for multi-project setups
-    if [ "${PROD_PROJECT}" != "${STAGE_PROJECT}" ]; then 
-        PROD_ENVIRONMENT_DIR=terraform/environments/prod
-        export TF_VAR_project_id=${PROD_PROJECT}
-        export TF_VAR_ops_project_id=${OPS_PROJECT}
-        terraform -chdir=${PROD_ENVIRONMENT_DIR} init
-        terraform -chdir=${PROD_ENVIRONMENT_DIR} import \
-            module.emblem_prod.google_storage_bucket.sessions \
-            "${PROD_PROJECT}-sessions" || true
-    fi
+    terraform -chdir=${PROD_ENVIRONMENT_DIR} init
+    terraform -chdir=${PROD_ENVIRONMENT_DIR} import \
+        module.emblem_prod.google_storage_bucket.sessions \
+        "${PROD_PROJECT}-sessions" || true
 fi
