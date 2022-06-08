efaf3ef361a2fef4f6564f6c0158497455cbef5e
Adam Ross <adamross@google.com>
2022-06-08 12:54:14 -0700

    fix: terraform style

--- terraform/environments/ops/main.tf
+++ terraform/environments/ops/main.tf
@@ -1,8 +1,8 @@
 module "emblem_ops" {
-  source     = "../../modules/ops"
-  project_id = var.project_id
-  region     = var.region
-  setup_cd_system  = var.setup_cd_system
-  repo_owner = var.repo_owner
-  repo_name  = var.repo_name
+  source          = "../../modules/ops"
+  project_id      = var.project_id
+  region          = var.region
+  setup_cd_system = var.setup_cd_system
+  repo_owner      = var.repo_owner
+  repo_name       = var.repo_name
 }
