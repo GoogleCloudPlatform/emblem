e2aaa588310ead97bf43b380ac45b57e5fd8ebad
Patti Shin <pattishin@users.noreply.github.com>
2022-07-10 20:46:07 -0700

    fix: adding in license headers

--- experimental/client-app/server/src/config.js
+++ experimental/client-app/server/src/config.js
@@ -1,6 +1,20 @@
-export const CLIENT_ID = process.env.CLIENT_ID;
-export const CLIENT_SECRET = process.env.CLIENT_SECRET;
-export const REDIRECT_URI = process.env.REDIRECT_URI;
-export const SITE_URL = process.env.SITE_URL;
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+export const { CLIENT_ID } = process.env;
+export const { CLIENT_SECRET } = process.env;
+export const { REDIRECT_URI } = process.env;
+export const { SITE_URL } = process.env;
 export const JWT_SECRET = process.env.JWT_SECRET || 'test_default_secret';
-export const COOKIE_NAME = "auth_token";
+export const COOKIE_NAME = 'auth_token';
--- experimental/client-app/server/src/index.js
+++ experimental/client-app/server/src/index.js
@@ -1,10 +1,24 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 /* eslint-disable */
-import express from "express";
-import jwt from "jsonwebtoken";
-import axios from "axios";
-import cors from "cors";
-import querystring from "querystring";
-import cookieParser from "cookie-parser";
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import axios from 'axios';
+import cors from 'cors';
+import querystring from 'querystring';
+import cookieParser from 'cookie-parser';
 import {
   CLIENT_ID,
   JWT_SECRET,
@@ -12,36 +26,32 @@ import {
   COOKIE_NAME,
   REDIRECT_URI,
   SITE_URL,
-} from "./config.js";
+} from './config.js';
 
 const port = 4000;
-const redirectURI = "/auth/google";
+const redirectURI = '/auth/google';
 const app = express();
 
-function getTokens({
-  code,
-  clientId,
-  clientSecret,
-  redirectUri,
-}) {
-  const url = "https://oauth2.googleapis.com/token";
+function getTokens({ code, clientId, clientSecret, redirectUri }) {
+  const url = 'https://oauth2.googleapis.com/token';
   const values = {
     code,
     client_id: clientId,
     client_secret: clientSecret,
     redirect_uri: redirectUri,
-    grant_type: "authorization_code",
+    grant_type: 'authorization_code',
   };
 
   const options = {
     headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
+      'Content-Type': 'application/x-www-form-urlencoded',
     },
   };
 
-  return axios.post(url, querystring.stringify(values), options)
-    .then((res) => res.data)
-    .catch((err) => {
+  return axios
+    .post(url, querystring.stringify(values), options)
+    .then(res => res.data)
+    .catch(err => {
       console.error(`Failed to fetch auth tokens`);
       throw new Error(err.message);
     });
@@ -51,7 +61,7 @@ function getTokens({
  * Applying cors
  * Origin: applies access-control-allow-origin to client app
  * Client: applies access-control-allow-credentials to true
- */ 
+ */
 app.use(
   cors({
     origin: SITE_URL,
@@ -63,7 +73,7 @@ app.use(cookieParser());
 
 /**
  * Handle Auth Callback
- */ 
+ */
 app.get(`${redirectURI}`, async (req, res) => {
   const code = req.query.code;
 
@@ -71,7 +81,7 @@ app.get(`${redirectURI}`, async (req, res) => {
     code,
     clientId: CLIENT_ID,
     clientSecret: CLIENT_SECRET,
-    redirectUri: `${REDIRECT_URI}`
+    redirectUri: `${REDIRECT_URI}`,
   });
 
   const userURL = `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${access_token}`;
@@ -81,9 +91,10 @@ app.get(`${redirectURI}`, async (req, res) => {
     },
   };
 
-  const user = await axios.get(userURL, options)
-    .then((res) => res.data)
-    .catch((err) => {
+  const user = await axios
+    .get(userURL, options)
+    .then(res => res.data)
+    .catch(err => {
       console.error(`Failed to fetch user`);
       throw new Error(err.message);
     });
@@ -101,47 +112,47 @@ app.get(`${redirectURI}`, async (req, res) => {
 
 /**
  * Retrieve logged in user profile
- */ 
-app.get("/getUser", (req, res) => {
+ */
+app.get('/getUser', (req, res) => {
   let user;
-  
+
   try {
     const cookie = req.cookies[COOKIE_NAME];
     user = jwt.verify(cookie, JWT_SECRET);
   } catch (err) {
     throw new Error(err);
   }
-  
+
   return res.send(user);
 });
 
 /**
  * Check for auth cookie
- */ 
-app.get("/hasCookie", (req, res) => {
+ */
+app.get('/hasCookie', (req, res) => {
   let cookie;
-  
+
   try {
     cookie = req.cookies[COOKIE_NAME];
   } catch (err) {
     throw new Error(err);
   }
-  
+
   return res.send(!!cookie);
 });
 
 /**
  * Handle auth logout
- */ 
-app.get("/logout", (req, res) => {
+ */
+app.get('/logout', (req, res) => {
   try {
-    if(!!req.cookies[COOKIE_NAME]) {
-      res.clearCookie(COOKIE_NAME); 
+    if (!!req.cookies[COOKIE_NAME]) {
+      res.clearCookie(COOKIE_NAME);
     }
   } catch (err) {
     throw new Error(error.message);
   }
-  
+
   res.send('logout completed');
 });
 
