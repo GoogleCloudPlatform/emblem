36c0f67e37871ef11c4a54fd410b54f3f1602f3b
ace-n <anassri@google.com>
2022-08-10 16:12:52 -0700

    Add gcloudignore for website build

--- setup.sh
+++ setup.sh
@@ -150,12 +150,14 @@ gcloud builds submit "content-api" \
     --config=ops/api-build.cloudbuild.yaml \
     --project="$OPS_PROJECT" --substitutions=_REGION="$REGION",SHORT_SHA="$SHORT_SHA"
 
-gcloud builds submit "website" \
+gcloud builds submit \
     --config=ops/web-build.cloudbuild.yaml \
+    --ignore-file=ops/web-build.gcloudignore
     --project="$OPS_PROJECT" --substitutions=_REGION="$REGION",SHORT_SHA="$SHORT_SHA"
 
 gcloud builds submit "ops/e2e-runner" \
     --config=ops/e2e-runner-build.cloudbuild.yaml \
+
     --project="$OPS_PROJECT" --substitutions=_REGION="$REGION",_IMAGE_TAG="$E2E_RUNNER_TAG"
 
 fi # skip build
