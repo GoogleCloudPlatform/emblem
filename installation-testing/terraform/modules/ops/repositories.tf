9252033cef4f8f61092e6da01da12b72b094eff2
ace-n <anassri@google.com>
2022-08-09 18:02:08 -0700

    Typo

--- installation-testing/terraform/modules/ops/repositories.tf
+++ installation-testing/terraform/modules/ops/repositories.tf
@@ -1,4 +1,4 @@
-resource "google_artifact_registry_repository" "e2e_testing_docker" {
+resource "google_artifact_registry_repository" "installation_testing_docker" {
   format        = "DOCKER"
   location      = var.region
   repository_id = "installation-testing"
