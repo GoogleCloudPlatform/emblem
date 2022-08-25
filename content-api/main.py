473c2ba594315ba23016017a99aee155ca23a123
Patti Shin <pattishin@users.noreply.github.com>
2022-08-25 09:37:32 -0700

    refactor: removing cors

--- content-api/main.py
+++ content-api/main.py
@@ -14,7 +14,6 @@
 
 from os import set_inheritable
 from flask import Flask, g, request
-from flask_cors import CORS
 from google.auth.transport import requests as reqs
 from google.oauth2 import id_token
 from resources import methods
@@ -28,7 +27,6 @@ resource = [
 ]
 
 app = Flask(__name__)
-cors = CORS(app)
 
 # Check authentication and remember result in global request context
 #
