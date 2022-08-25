f6b876417c19d2bdc3de28653e019e1d356b2969
Patti Shin <pattishin@users.noreply.github.com>
2022-08-25 08:50:43 -0700

    refactor: adding comment

--- content-api/main.py
+++ content-api/main.py
@@ -15,12 +15,9 @@
 from os import set_inheritable
 from flask import Flask, g, request
 from flask_cors import CORS
-
 from google.auth.transport import requests as reqs
 from google.oauth2 import id_token
-
 from resources import methods
-
 
 resource = [
     "approvers",
@@ -31,7 +28,7 @@ resource = [
 ]
 
 app = Flask(__name__)
-cors = CORS(app)
+cors = CORS(app) # add cross-origin
 
 # Check authentication and remember result in global request context
 #
