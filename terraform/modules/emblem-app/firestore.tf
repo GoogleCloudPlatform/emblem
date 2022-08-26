c6b0153a7364903253269408a8d84a7bebfbcf57
Roger Martinez <31829545+rogerthatdev@users.noreply.github.com>
2022-08-26 11:54:00 -0700

    refactor: Terraform to seed approver and donors test data to Firestore (#644)
    * add terraform to seed approver to firebase
    
    * create template dir
    
    * move approvers template file
    
    * terraformize donors test data
    
    * incorporate bool toggle for seeding test data
    
    * add missing count.index to document_id
    
    * correction to test data file path
    
    * add project parameter to firestore resources
    
    * terraform fmt
    
    Co-authored-by: Roger Martinez <rogerthatdev@gmail.com>

---
+++ terraform/modules/emblem-app/firestore.tf
@@ -0,0 +1,49 @@
+locals {
+  donor_test_data = jsondecode(file("${path.module}/files/test-data/donors.json"))
+}
+
+# Seed Firestore with approvers
+resource "random_string" "approver_doc_id" {
+  count       = var.seed_test_data ? 1 : 0
+  length      = 20
+  special     = false
+  min_numeric = 3
+  min_lower   = 10
+}
+
+data "template_file" "approver" {
+  count    = var.seed_test_data ? 1 : 0
+  template = file("${path.module}/files/templates/approvers.tftpl")
+  vars = {
+    email = var.approver_email
+  }
+}
+
+resource "google_firestore_document" "approvers" {
+  project     = var.project_id
+  count       = var.seed_test_data ? 1 : 0
+  collection  = "approvers"
+  document_id = random_string.approver_doc_id[count.index].result
+  fields      = data.template_file.approver[count.index].rendered
+}
+
+# Seed test data to donors collection
+
+data "template_file" "donors" {
+  template = file("${path.module}/files/templates/donors.tftpl")
+  count    = var.seed_test_data ? "${length(local.donor_test_data)}" : 0
+  vars = {
+    id              = local.donor_test_data[count.index].id
+    name            = local.donor_test_data[count.index].data.name
+    email           = local.donor_test_data[count.index].data.email
+    mailing_address = local.donor_test_data[count.index].data.mailing_address
+  }
+}
+
+resource "google_firestore_document" "donors" {
+  project     = var.project_id
+  count       = var.seed_test_data ? "${length(data.template_file.donors)}" : 0
+  collection  = "donors"
+  document_id = local.donor_test_data[count.index].id
+  fields      = data.template_file.donors[count.index].rendered
+}
