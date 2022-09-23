6aab0bb9671e7b9a3d9d369d6335c4ee798d471b
Roger Martinez <rogerthatdev@gmail.com>
2022-09-23 13:13:39 -0700

    disable e2e build triggers

--- terraform/modules/ops/testing.tf
+++ terraform/modules/ops/testing.tf
@@ -85,7 +85,7 @@ resource "google_cloudbuild_trigger" "web_system_tests" {
 # the API.  Investigate work-around.
 resource "google_cloudbuild_trigger" "e2e_nightly_tests" {
   project = var.project_id
-  count   = var.setup_cd_system ? 1 : 0
+  count   = 0
   name    = "e2e-nightly-tests"
 
   pubsub_config {
@@ -111,3 +111,28 @@ resource "google_cloudbuild_trigger" "e2e_nightly_tests" {
   tags           = []
 }
 
+resource "google_cloudbuild_trigger" "e2e_testing_build_runner" {
+  project  = var.project_id
+  count    = 0
+  name     = "e2e-runner-push-to-main"
+  filename = "ops/e2e-runner-build.cloudbuild.yaml"
+  included_files = [
+    "website/e2e-test/*",
+  ]
+  github {
+    owner = var.repo_owner
+    name  = var.repo_name
+    # NOTE: this image will ONLY be updated when a PR
+    # is merged into `main`. "Presubmit only" changes
+    # within a non-merged PR will NOT be included!
+    push {
+      branch = "^main$"
+    }
+  }
+
+  # These properties are detected as changed if not initialized.
+  # Alternately, add a lifecycle rule to ignore_changes.
+  ignored_files = []
+  substitutions = {}
+  tags          = []
+}
