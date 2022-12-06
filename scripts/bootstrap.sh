01700e2b7e52f7dbf75e89bbccead54df51b53a1
Roger Martinez <rogerthatdev@gmail.com>
2022-12-06 10:17:03 -0800

    add missing iam to bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 ### Bootstrap
-
+# Services
+gcloud services enable iamcredentials.googleapis.com --project $OPS_PROJECT
 # Create terraform service account
 
 gcloud iam service-accounts create emblem-terraformer \
@@ -11,13 +12,14 @@ gcloud iam service-accounts create emblem-terraformer \
 
 # Give cloud build token creator
 OPS_PROJECT_NUMBER=$(gcloud projects list --format='value(PROJECT_NUMBER)' --filter=PROJECT_ID=$OPS_PROJECT)
-gcloud iam service-accounts add-iam-policy-binding \
+
+gcloud iam service-accounts add-iam-policy-binding --project=$OPS_PROJECT \
     emblem-terraformer@${OPS_PROJECT}.iam.gserviceaccount.com \
     --member="serviceAccount:${OPS_PROJECT_NUMBER}@cloudbuild.gserviceaccount.com" \
     --role="roles/editor"
-
+ 
 # Apply IAM permissions for terraformer to all projects
-# Editor and IAM Security admin for OPS
+# Editor and IAM Security admin and storage admin for OPS
 gcloud projects add-iam-policy-binding $OPS_PROJECT \
     --member=serviceAccount:emblem-terraformer@${OPS_PROJECT}.iam.gserviceaccount.com \
     --role="roles/editor"
@@ -26,6 +28,10 @@ gcloud projects add-iam-policy-binding $OPS_PROJECT \
     --member=serviceAccount:emblem-terraformer@${OPS_PROJECT}.iam.gserviceaccount.com \
     --role="roles/iam.securityAdmin"
 
+gcloud projects add-iam-policy-binding $OPS_PROJECT \
+    --member=serviceAccount:emblem-terraformer@${OPS_PROJECT}.iam.gserviceaccount.com \
+    --role="roles/storage.admin"
+    
 # editor for STAGE and PROD
 gcloud projects add-iam-policy-binding $STAGE_PROJECT \
     --member=serviceAccount:emblem-terraformer@${OPS_PROJECT}.iam.gserviceaccount.com \
