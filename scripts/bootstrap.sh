c3624d0d84f2289da55a1467d89dceffd8102919
Roger Martinez <rogerthatdev@gmail.com>
2022-12-08 16:03:47 -0800

    fix URL

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,29 +4,29 @@ set -eu
 OPS_PROJECT_NUMBER=$(gcloud projects list --format='value(PROJECT_NUMBER)' --filter=PROJECT_ID=$OPS_PROJECT)
 # Services needed for Terraform to manage resources via service account 
 
-echo -e "Enabling initial required services... \n"
-gcloud services enable --project $OPS_PROJECT --async \
-    iamcredentials.googleapis.com \
-    cloudresourcemanager.googleapis.com \
-    serviceusage.googleapis.com \
-    appengine.googleapis.com \
-    cloudbuild.googleapis.com
+# echo -e "Enabling initial required services... \n"
+# gcloud services enable --project $OPS_PROJECT --async \
+#     iamcredentials.googleapis.com \
+#     cloudresourcemanager.googleapis.com \
+#     serviceusage.googleapis.com \
+#     appengine.googleapis.com \
+#     cloudbuild.googleapis.com
     
-# Create terraform service account
+# # Create terraform service account
 
-echo -e "Creating Emblem Terraform service account... \n"
-gcloud iam service-accounts create emblem-terraformer \
-    --project="$OPS_PROJECT" \
-    --description="Service account for deploying resources via Terraform" \
-    --display-name="Emblem Terraformer"
+# echo -e "Creating Emblem Terraform service account... \n"
+# gcloud iam service-accounts create emblem-terraformer \
+#     --project="$OPS_PROJECT" \
+#     --description="Service account for deploying resources via Terraform" \
+#     --display-name="Emblem Terraformer"
 
-# Give cloud build service account token creator on terraform service account policy
+# # Give cloud build service account token creator on terraform service account policy
 
-echo -e "Setting assigning required IAM roles... \n"
-gcloud iam service-accounts add-iam-policy-binding --project=$OPS_PROJECT \
-    emblem-terraformer@${OPS_PROJECT}.iam.gserviceaccount.com \
-    --member="serviceAccount:${OPS_PROJECT_NUMBER}@cloudbuild.gserviceaccount.com" \
-    --role="roles/iam.serviceAccountTokenCreator"
+# echo -e "Setting assigning required IAM roles... \n"
+# gcloud iam service-accounts add-iam-policy-binding --project=$OPS_PROJECT \
+#     emblem-terraformer@${OPS_PROJECT}.iam.gserviceaccount.com \
+#     --member="serviceAccount:${OPS_PROJECT_NUMBER}@cloudbuild.gserviceaccount.com" \
+#     --role="roles/iam.serviceAccountTokenCreator"
 
 # Ops permissions
 
@@ -119,7 +119,7 @@ gcloud storage buckets add-iam-policy-binding gs://$STATE_GCS_BUCKET_NAME \
 # Add GitHub repo to ops project
 REPO_CONNECT_URL="https://console.cloud.google.com/cloud-build/triggers/connect?project=${OPS_PROJECT}"
 
-echo -e "Connect a fork of the Emblem GitHub repo to your ops project via the Cloud Console: $URL \n"
+echo -e "Connect a fork of the Emblem GitHub repo to your ops project via the Cloud Console: \n $(tput bold)$REPO_CONNECT_URL$(tput sgr0) \n"
 read -n 1 -r -s -p $'Once your forked Emblem repo is connected, please type any key to continue.\n'
 
 continue=1
