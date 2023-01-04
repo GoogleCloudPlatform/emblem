325f0495564e3a4a0d3e74bf1699e35ee1b91b40
ace-n <anassri@google.com>
2023-01-04 14:05:05 -0800

    ADDRESS COMMENT: move Cloud Run IAM role from ops_iam -> app_iam

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -40,9 +40,6 @@ STATE_GCS_BUCKET_NAME="$OPS_PROJECT-tf-states"
 OPS_IAM="bindings:
 - members:
   - serviceAccount:${EMBLEM_TF_SERVICE_ACCOUNT}
-  role: roles/run.developer
-- members:
-  - serviceAccount:${EMBLEM_TF_SERVICE_ACCOUNT}
   role: roles/cloudbuild.builds.editor
 - members:
   - serviceAccount:${EMBLEM_TF_SERVICE_ACCOUNT}
@@ -78,6 +75,9 @@ APP_IAM="bindings:
 - members:
   - serviceAccount:${EMBLEM_TF_SERVICE_ACCOUNT}
   role: roles/iam.serviceAccountAdmin
+- members:
+  - serviceAccount:${EMBLEM_TF_SERVICE_ACCOUNT}
+  role: roles/run.developer
 - members:
   - serviceAccount:${EMBLEM_TF_SERVICE_ACCOUNT}
   role: roles/firebase.managementServiceAgent"
