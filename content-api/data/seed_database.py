ea191d50ee7ead3f0dbcf8448ec29bc1feefa923
Aaron Gabriel Neyer <aarongabriel@google.com>
2023-01-05 12:39:28 -0700

    feat: add in unseed database functionality for emblem cleanup (#751)
    * feat: add in unseed database functionality for emblem cleanup
    
    * commit setup.sh, and lint files
    
    * replace testing echo
    
    * add newline
    
    Co-authored-by: Roger Martinez <rogerthatdev@gmail.com>

--- content-api/data/seed_database.py
+++ content-api/data/seed_database.py
@@ -12,9 +12,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import argparse
+import json
 from google.cloud import firestore
 
-import json
+
+def seed_approver(email):
+    client = firestore.Client()
+    print("Seeding approver into Google Cloud Project '{}'.".format(client.project))
+
+    approver = {
+        "kind": "approvers",
+        "email": email,
+        "active": True,
+        "name": "Seeded test user",
+    }
+
+    doc_ref = client.collection("approvers").document()
+    doc_ref.set(approver)
 
 
 def seed_database(content):
@@ -33,7 +48,37 @@ def seed_database(content):
     print("Successfully seeded database")
 
 
-with open("sample_data.json", "r") as f:
-    seed_content = json.load(f)
+def unseed_database():
 
-seed_database(seed_content)
+    client = firestore.Client()
+    print("Deleting seed data from Google Cloud Project '{}'.".format(client.project))
+    print("This may take a few minutes...")
+    # Delete all objects from seed data
+    collections = ["approvers", "campaigns", "causes", "donations", "donors"]
+    for coll in collections:
+        coll_ref = client.collection(coll)
+        docs = coll_ref.list_documents()
+        for doc in docs:
+            doc.delete()
+
+    print("Finished unseeding database")
+
+
+parser = argparse.ArgumentParser(description="Seed or unseed firestore database")
+parser.add_argument("seed_or_unseed")
+parser.add_argument("email", help="email for approver", nargs="?")
+
+args = parser.parse_args()
+
+if args.seed_or_unseed == "unseed":
+    unseed_database()
+elif args.seed_or_unseed == "seed":
+    if not args.email:
+        print("email must be provided as the second arg")
+    else:
+        seed_approver(args.email)
+        with open("sample_data.json", "r") as f:
+            seed_content = json.load(f)
+        seed_database(seed_content)
+else:
+    print('first arg must be specified as "seed" or "unseed"')
